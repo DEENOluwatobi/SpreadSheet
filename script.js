e3d04a4b4dd29e1e7d04e52ef44524d6e0cc76d6
DEENOluwatobi <shodimutobie@gmail.com>
2023-10-20 21:31:39 +0100

    made changes

--- script.js
+++ script.js
@@ -31,6 +31,20 @@ const alphabets = [
     "Y",
     "Z",
     "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
 ]
 class Cell {
     constructor(isHeader, disabled, data, row, column, rowName, columnName, active = false) {
